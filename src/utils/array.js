83ca7a459a6c9fef014d34666a6a99710f86471a
Yaswanth Goli <dev.yashu58@hotmail.com>
2017-04-24 16:14:53 +0530

    Making array length cached

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,5 +1,6 @@
 export function find ( array, fn ) {
-	for ( let i = 0; i < array.length; i += 1 ) {
+	let arrLen = array.length;
+	for ( let i = 0; i < arrLen; i += 1 ) {
 		if ( fn( array[i], i ) ) return array[i];
 	}
 
